acec9fcdc75083c447a88c4756712ba63f465c0b
Daniel Westover <danwestover@users.noreply.github.com>
2018-12-03 14:45:57 -0500

    redirect user if profile not exist
    if the ?user=username username does not exist in the database, redirect to an error page (temporarily just index).

--- rideresult.php
+++ rideresult.php
@@ -13,40 +13,43 @@
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="resources/style.css" rel="stylesheet" type="text/css">
-  
-  </head>
-  <body>
-      <?php include 'nav.php'; ?>
-     
-   
-   <h1 id="title"><strong>rEDUce</strong></h1>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+            crossorigin="anonymous">
 
-   <div class="content">
-      <!-- notification message -->
-      <?php if (isset($_SESSION['success'])) : ?>
-        <div class="error success" >
-          <h3>
-            <?php 
+        <link href="resources/style.css" rel="stylesheet" type="text/css">
+
+    </head>
+
+    <body>
+        <?php include 'nav.php'; ?>
+
+
+        <h1 id="title"><strong>rEDUce</strong></h1>
+
+        <div class="content">
+            <!-- notification message -->
+            <?php if (isset($_SESSION['success'])) : ?>
+            <div class="error success">
+                <h3>
+                    <?php 
               echo $_SESSION['success']; 
               unset($_SESSION['success']);
             ?>
-          </h3>
+                </h3>
+            </div>
+            <?php endif ?>
+
+            <!-- logged in user information -->
+
         </div>
-      <?php endif ?>
-  
-      <!-- logged in user information -->
-      
-  </div>
-<?php
+        <?php
   $server = 'localhost';
   $user = 'root';
   $pass = '';
@@ -98,19 +101,26 @@
   }
 
 ?>
-  <div class = "results">
-    <ul class="list-unstyled mt-3 mb-4">
-      <li>30 users included</li>
-      <li>30 users included</li>
-      <li>30 users included</li>
-      
-    </ul> 
-  </div>
-    
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+        <div class="results">
+            <ul class="list-unstyled mt-3 mb-4">
+                <li>30 users included</li>
+                <li>30 users included</li>
+                <li>30 users included</li>
+
+            </ul>
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous">
+        </script>
+    </body>
+
 </html>
