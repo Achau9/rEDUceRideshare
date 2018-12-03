a105ac8d73e8255885ef132b10419ffb0ad6f6e1
Michael Biscotti <biscom@rpi.edu>
2018-12-03 13:20:49 -0500

    Updated changes to profile

---
+++ profile_query.php
@@ -0,0 +1,30 @@
+<!-- <div class="HistoryOutput">
+ -->  
+<!-- </div> -->
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rideshare";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// $sql = "SELECT all comments for specified user";
+$sql = "SELECT * FROM comments c WHERE EXISTS(SELECT * FROM users u WHERE c.ReviewedUser = u.username)";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output comments in each row and calculate star rating
+    while($row = $result->fetch_assoc()) {
+        echo $row["ReviewPoster"] . ": " . $row["StarRating"] . " Stars" . <br> . "\t " . $row["TextReview"];
+    }
+} else {
+    echo "No comments exist yet for this user";
+}
+$conn->close();
+?>
