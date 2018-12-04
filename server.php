c49a25a12622f240e63e985a242571d85a18fe62
Daniel Westover <danwestover@users.noreply.github.com>
2018-12-04 00:48:45 -0500

    Update server.php
    oops.

--- server.php
+++ server.php
@@ -54,7 +54,7 @@ if (isset($_POST['reg_user'])) {
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
-  	// header('location: index.php');
+  	header('location: index.php');
   }
 }
 // LOGIN USER
