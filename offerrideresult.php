3b86ebf39d63d49c7cb83018ed35759eb06e3b44
Daniel Westover <danwestover@users.noreply.github.com>
2018-12-03 19:32:08 -0500

    fixed offerrideresult btns.

--- offerrideresult.php
+++ offerrideresult.php
@@ -13,25 +13,28 @@
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="resources/style.css" rel="stylesheet" type="text/css">
-  
-  </head>
-  <body>
-      <?php include 'nav.php'; ?>
-     
-   
-   <h1 id="title"><strong>rEDUce</strong></h1>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+            crossorigin="anonymous">
 
-   
-<?php
+        <link href="resources/style.css" rel="stylesheet" type="text/css">
+
+    </head>
+
+    <body>
+        <?php include 'nav.php'; ?>
+
+
+        <h1 id="title"><strong>rEDUce</strong></h1>
+
+
+        <?php
   $server = 'localhost';
   $user = 'root';
   $pass = '';
@@ -69,11 +72,10 @@
       // var_dump($result);
       foreach($result as $value){
         echo("<div class = \"results\"><ul class=\"list-unstyled mt-3 mb-4\">");
-        echo("<li><h3>Riders:$value[username]</h3></li><li>Departure Date: $value[date]</li><li>Destination: $value[city], $value[state]</li></ul><button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\">
-            <a href =\"profile.php?user=$value[username]\">View Profile<a>
-          </button><button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\"name=\"accept\"'>
-            <a href ='offerrideresult.php?rideid=".$value["rideid"]."&state=".$state."&city=".$city."&date=".$r_date."'>Accept Ride</a>
-          </button></div>");
+        echo("<li><h3>Riders:$value[username]</h3></li><li>Departure Date: $value[date]</li><li>Destination: $value[city], $value[state]</li></ul>
+          <a class='btn btn-outline-success my-2 my-sm-0' href =\"profile.php?user=$value[username]\">View Profile</a>
+          <a class='btn btn-outline-success my-2 my-sm-0' href ='offerrideresult.php?rideid=".$value["rideid"]."&state=".$state."&city=".$city."&date=".$r_date."'>Accept Ride</a>
+          </div>");
         
       }
 
@@ -127,12 +129,19 @@
   }
 
 ?>
-  
-    
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous">
+        </script>
+    </body>
+
 </html>
